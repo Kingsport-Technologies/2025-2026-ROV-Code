c3446b2e3ed738cd4c5ff3c0a309d3392160dc32
William Ewing <ewingwm>
2026-03-03 16:40:52 -0500

    Fix top computer and update makefile

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # Define variables (conventionally in uppercase)
 CXX = g++
 TARGET = topend
-OBJS = topcomputer.o pwmmath.o websocketserver.o
+OBJS = topcomputer.o pwmmath.o websocketserver.o outgoingserver.o
 
 # The default target (often 'all') builds the program
 all: $(TARGET)
@@ -16,6 +16,8 @@ pwmmath.o: pwmmath.cpp pwmmath.hpp
 	$(CXX) -c pwmmath.cpp
 websocketserver.o: websocketserver.cpp websocketserver.hpp pwmmath.hpp
 	$(CXX) -c websocketserver.cpp
+outgoingserver.o: outgoingserver.cpp outgoingserver.hpp
+	$(CXX) -c outgoingserver.cpp
 # Phony target to clean up generated files
 clean:
 	rm -f $(TARGET) $(OBJS)
--- topcomputer.cpp
+++ topcomputer.cpp
@@ -5,11 +5,24 @@
 #include <unistd.h>
 #include <chrono>
 #include "websocketserver.hpp"
+#include "outgoingserver.hpp"
+#include "json.hpp"
+#include <functional>
 
 using namespace std;
-WebSocketServer server_rx;
+using json = nlohmann::json;
+typedef websocketpp::server<websocketpp::config::asio> server;
 
+WebSocketServer server_rx;
+outgoingserver server_tx;
 int main(int argc, char* argv[])
 {
+    server_rx.send_callback(
+        [](nlohmann::json content) {
+            server_tx.sendMessage(content.dump());
+        }
+    );
+
     server_rx.start_server();
+    server_tx.start(argv[1], std::stoi(argv[2]));
 }
